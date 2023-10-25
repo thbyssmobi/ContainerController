abbb7b62277915649aad8e9ba16ec3ffcbad379a
thbyssmobi <131761689+thbyssmobi@users.noreply.github.com>
2023-10-25 11:59:15 +0200

    Update ContainerControllerDelegate.swift

--- Sources/ContainerController/ContainerControllerDelegate.swift
+++ Sources/ContainerController/ContainerControllerDelegate.swift
@@ -20,24 +20,26 @@ public protocol ContainerControllerDelegate {
     /// Reports the changes current position of the container, after its use
     func containerControllerMove(_ containerController: ContainerController, position: CGFloat, type: ContainerMoveType, animation: Bool)
     
-    func hideMenu(position: Double)
-    
 }
 
 @available(iOS 13.0, *)
 public extension ContainerControllerDelegate {
     
     func containerControllerRotation(_ containerController: ContainerController) {
+        
+        if UIDevice.current.userInterfaceIdiom == .pad  {
+            let width = UIScreen.main.bounds.height
+            let frame = CGRect(x: 0, y: 0, width: 400, height: containerController.deviceHeight * 2)
+            containerController.view.refreshContentView(frame: frame)
+            containerController.layout.insets = ContainerInsets(right: width - 400 - 10, left: 10)
+            containerController.callBack?()
+        }
     }
-    
     
     func containerControllerShadowClick(_ containerController: ContainerController) {
     }
     
     func containerControllerMove(_ containerController: ContainerController, position: CGFloat, type: ContainerMoveType, animation: Bool) {
-        
-        hideMenu(position: position)
-        
     }
 }
 
